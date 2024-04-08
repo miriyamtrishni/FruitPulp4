23bfecf154b658e373db0aad31a1188e940e7bfe
shainiRose <shainiu22@gmail.com>
2024-04-08 11:11:26 +0530

    search,report

---
+++ server/models/DeletedUsers.js
@@ -0,0 +1,64 @@
+// DeletedUsers.js
+const mongoose = require('mongoose');
+
+const DeletedUserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    // Add other fields as needed, similar to UserModel
+    eid:{
+        type:String,
+        required:true,
+        maxlength: 3,
+        minlength: 3
+    },
+
+    nic:{
+        type:String,
+        required:true,
+        maxlength: 12,
+        minlength: 12
+    },
+
+    gender:{
+        type:String,
+        required:true
+    },
+
+    age:{
+        type:Number,
+        required:true
+    },
+
+    address:{
+        type:String,
+        required:true
+    },
+
+   
+
+    email:{
+        type:String,
+        required:true
+    },
+
+    jobtitle:{
+        type:String,
+        required:true
+    },
+
+    salary:{
+        type:Number,
+        required:true
+    },
+
+   
+    deletedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const DeletedUserModel = mongoose.model('deleted_users', DeletedUserSchema);
+module.exports = DeletedUserModel;
