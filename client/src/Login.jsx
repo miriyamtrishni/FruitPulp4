24e452bb498fb5d228aa923c25319d3bd1ca192e
Shashi Wanniarachchi <w.sithumini21@gmail.com>
2024-04-14 15:54:52 +0530

    login css changed

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -23,76 +23,74 @@ function Login() {
             .catch(err => console.log(err));
     };
 
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    };
-
-    const handleMouseLeave = () => {
-        setIsHovered(false);
+    const handleRegisterClick = () => {
+        navigate('/register');
     };
 
     return (
         <div style={{
-            backgroundImage: 'url("/image/im2.jpg")',
-            backgroundSize: "cover",
-            backgroundPosition: "center",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh"
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#ffffff"
         }}>
-            <div style={{ border: "1px solid #ccc", padding: "20px", borderRadius: "5px", width: "300px", backgroundColor: "#FFAC1C" }}>
-                <h2>Login</h2>
+            <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+            <div style={{ border: "none", padding: "20px", borderRadius: "10px", display: "flex", flexDirection: "column", alignItems: "center", width: "80%",maxWidth: "700px", boxShadow: "0 4px 8px rgba(0,0,0,0.3)",fontFamily: 'Poppins, sans-serif' }}>
+                <h2 style={{color:"#F4BB29"}}>Login</h2>
+                <div style={{ display: "flex", width: "100%",marginLeft:"20px" }}>
+            <div style={{ flex: 1, marginBottom: "20px" }}>
                 <form onSubmit={handleSubmit}>
-                    <div style={{ marginBottom: "20px", display: "flex", flexDirection: "column" }}>
+                    <div style={{ marginBottom: "20px" }}>
                         <label htmlFor="email" style={{ marginBottom: "5px" }}>
-                            <strong>Email</strong>
+                        <i className="fa fa-envelope"></i> 
                         </label>
                         <input
                             type="text"
                             placeholder="Enter Email"
                             autoComplete="off"
                             name="email"
-                            style={{ marginTop: "5px" }}
+                            style={{ marginLeft: "30px" , width: "70%",padding: "8px"}}
                             onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
-                    <div style={{ marginBottom: "20px", display: "flex", flexDirection: "column" }}>
+                    <div style={{ marginBottom: "20px" }}>
                         <label htmlFor="password" style={{ marginBottom: "5px" }}>
-                            <strong>Password</strong>
+                        <i className="fa fa-lock"></i> 
                         </label>
                         <input
                             type="password"
                             placeholder="Enter Password"
                             autoComplete="off"
                             name="password"
-                            style={{ marginTop: "5px" }}
+                            style={{ marginLeft: "30px" , width: "70%",padding: "8px" }}
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <button type="submit" style={{
-                        backgroundColor: "green", color: "white", padding: "10px 20px", border: "none",
-                        borderRadius: "5px", cursor: "pointer", marginRight: "10px"
+                        backgroundColor: "#F4BB29", color: "white", padding: "10px 20px", border: "none",
+                        borderRadius: "5px", cursor: "pointer", width:"30%",marginLeft: "120px",fontWeight:"700",marginTop: "10px"
                     }}>
                         Login
                     </button>
                 </form>
-                <p>Already have an account</p>
-                <Link
-                    to="/register"
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    style={{
-                        backgroundColor: isHovered ? "lightgreen" : "transparent",
-                        border: "1px solid green", color: isHovered ? "green" : "#006400", padding: "10px 20px",
-                        borderRadius: "5px", cursor: "pointer", transition: "background-color 0.3s ease",
-                        textDecoration: "none",
-                    }}
-                >
-                    Register
-                </Link>
+                <p style={{ textAlign: "center", marginTop: "30px", marginBottom: "10px", fontFamily: 'Poppins, sans-serif',fontWeight:"500" ,marginLeft:"10px",fontSize:"15px"}}>    Don't have an account? Sign Up</p>
+                        <button onClick={handleRegisterClick} style={{
+                            backgroundColor: "#F4BB29", color: "white", padding: "10px 20px", border: "none",
+                            borderRadius: "5px", cursor: "pointer", width:"30%",fontWeight:"700",marginLeft:"120px"
+                        }}>
+                            Sign Up
+                        </button>
+                    </div>
+                    <div style={{ flex: 1 }}>
+                        <img src="/image/b4.jpg" alt="Login Image" style={{ width: "100%", borderRadius: "5px", height:"100%"}} />
+                    </div>
+                </div>
             </div>
         </div>
+        
     );
 }
 
